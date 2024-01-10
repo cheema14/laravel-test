b6f7dee652214fa29e0eff2553b0c9f19842083d
ali hassan <ali.hassan@pitb.gov.pk>
2024-01-11 01:54:30 +0500

    about me image modified

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -24,9 +24,10 @@
         </div>
     </nav>
 
+
     <div class="container py-4">
         <div class="card">
-            <img src="https://placehold.co/1100x400" class="card-img-top" alt="">
+            <img src="https://i.ibb.co/ZgTqG8X/slide4.png" class="card-img-top" alt="">
             <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
